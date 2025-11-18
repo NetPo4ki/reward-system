2e282286d328f34fea84a49b7bc48fe85d657d21
NetPo4ki <egoroneplusovich@gmail.com>
2025-11-18 22:58:47 +0300

    feat(server): router and healthcheck

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/NetPo4ki/reward-system/internal/config"
+	"github.com/NetPo4ki/reward-system/internal/server"
+)
+
+func main() {
+	cfg := config.FromEnv()
+	r := server.NewRouter(cfg)
+	addr := ":" + cfg.Port
+	log.Printf("listening on %s", addr)
+	if err := http.ListenAndServe(addr, r); err != nil {
+		log.Fatal(err)
+	}
+}
